1ae9c2f4a55b9656f9ed3c427c9f9c27a900ca51
priyanka1001 <67691762+priyanka1001@users.noreply.github.com>
2020-07-01 17:39:04 +0530

    Add files via upload

---
+++ Author.java
@@ -0,0 +1,14 @@
+package EncapAbs;
+
+public class Author {
+
+		public static String email;
+		public static char gender;
+		public static String name;
+		Author(String n,String e,char g)
+		{
+		        name=n;
+		        email=e;
+		        gender=g;
+		}
+	}
